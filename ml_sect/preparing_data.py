62b8d9aacafb155f1226a26fb7991448cf937a56
Suslov Mikhail <misha.suslov.2016@mail.ru>
2021-10-19 19:15:39 +0300

    Revert "Revert "feat: add class linreg and scoring""
    This reverts commit 1e23ee9efd3c047e4178af2f3c115f5e145eec05.

---
+++ ml_sect/preparing_data.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+from typing import Tuple
+from pandas.api.types import is_string_dtype, is_numeric_dtype
+from sklearn.preprocessing import LabelEncoder
+
+
+def prepare_df(df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    num_features = []
+    cat_features = []
+    for column in df:
+        if is_numeric_dtype(df[column]):
+            num_features.append(column)
+        elif is_string_dtype(df[column]):
+            cat_features.append(column)
+    df['expenses'] = np.log2(df['expenses'] + 1)
+    for feat in cat_features:
+        df[feat] = LabelEncoder().fit_transform(df[feat])
+    X = df.drop(columns=['expenses'], axis=1)
+    y = df['expenses']
+    return X, y
